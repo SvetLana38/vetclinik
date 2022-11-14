48882ea49d58fb8b6bf95b39c7f829919916e5e4
SvetLana38 <90665203+SvetLana38@users.noreply.github.com>
2022-11-14 23:33:22 +0800

    Add files via upload
    Сделана главная страница

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Админка</title>
+  <link rel="stylesheet" type="text/css" href="styles/style.css">
+</head>
+<body>
+    <header class="header">
+        <nav class="menu">
+            <ul class="topnav">
+                <li><a class="active" href="#home">Главная</a></li>
+                <li><a href="#news">Услуги и цены</a></li>
+                <li><a href="#contact">Специалисты</a></li>
+                <li><a href="#contact">Новости</a></li>
+                <li><a href="#contact">Контакты</a></li>
+
+                <li class="right"><a href="#about">наши филиалы</a></li>
+            </ul>
+        </nav>
+    </header>
+<section class="logo">
+<div style="padding:0 16px;">
+<img src="images/logo.png" alt="1">
+</div>
+</section>
+<section class="main">
+    <h1> Круглосуточная <br> ветеринарная клиника </h1>
+    <p> У нас вы можете записаться на приём по <br>
+телефону и получить консультацию </p>
+<div style="display: flex;">
+    <div style="padding-left: 1%;"> <img src="images/logo.png" alt="1"> </div>
+    <div style="padding-left: 1%;"> <img src="images/logo.png" alt="1"> </div>
+    <div style="padding-left: 1%;"> <img src="images/logo.png" alt="1"> </div>
+</div>
+</section>
+<section class="service">
+<div class="container">
+            <div class="rows">
+           
+                <div class="row">
+                    <img src="images/logo.png" alt="">
+                    <h3>Name 1</h3>
+                </div>
+                <div class="row">
+                    <img src="images/logo.png" alt="">
+                    <h3>Name 2</h3>
+                </div>
+                <div class="row">
+                    <img src="images/logo.png" alt="">
+                    <h3>Name 3</h3>
+                </div>
+                <div class="row">
+                    <img src="images/logo.png" alt="">
+                    <h3>Name 4</h3>
+                </div>           
+            </div>
+        </div>
+</section>
+</body>
+</html>
